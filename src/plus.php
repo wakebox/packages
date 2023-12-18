9f49d637bcdef1fa2c2c76effabbf18373299663
wilson <wilson_chu@newsoft.com.tw>
2023-12-18 17:48:04 +0800

    first

---
+++ src/plus.php
@@ -0,0 +1,11 @@
+<?php namespace Npack\Hello; 
+  class Plus {
+  public function __construct($a=””, $b=””)
+  {
+    $this->result = $a + $b;
+  }
+  public function result()
+  {
+    return $this->result;
+  }
+}
